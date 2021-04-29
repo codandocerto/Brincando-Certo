f81ae5c1ea343fc436b969738770b77fff5f1e0b
Codando Certo <80582364+codandocerto@users.noreply.github.com>
2021-04-29 12:18:00 -0300

    Create IA Dinossauro da Google.js

---
+++ IA Dinossauro da Google.js
@@ -0,0 +1,47 @@
+//aumentar velocidade:
+//setInterval(() => this.window.Runner.instance_.currentSpeed += 0.5, 1000)
+
+var up = new KeyboardEvent('keydown', { keyCode: 38 })
+var space = new KeyboardEvent('keydown', { keyCode: 32 })
+var down = new KeyboardEvent('keydown', { keyCode: 40 })
+var updown = new KeyboardEvent('keyup', { keyCode: 40 })
+
+var lastDown = false
+var method = function () {
+	var gameSpeed = this.window.Runner.instance_.currentSpeed
+
+	var trex = {
+		height: this.window.Runner.instance_.tRex.config.HEIGHT,
+		xPos: this.window.Runner.instance_.tRex.xPos,
+	}
+
+	var obstacles = this.window.Runner.instance_.horizon.obstacles
+
+	var nextObs = obstacles.find(
+		x => x.xPos - trex.xPos > 0 && x.xPos - trex.xPos < 12 * gameSpeed
+	)
+
+	var oldObject = obstacles.find(x => x.xPos - trex.xPos < 10)
+
+	if (nextObs) {
+		if (nextObs.typeConfig.type === 'PTERODACTYL') {
+			if (nextObs.yPos === 100) {
+				lastDown = false
+				document.dispatchEvent(updown)
+				document.dispatchEvent(up)
+			} else {
+				lastDown = true
+				document.dispatchEvent(down)
+			}
+		} else {
+			lastDown = false
+			document.dispatchEvent(updown)
+			document.dispatchEvent(up)
+		}
+	} else if (!oldObject && !lastDown) {
+		lastDown = true
+		document.dispatchEvent(down)
+	}
+}
+
+setInterval(method, 15)
